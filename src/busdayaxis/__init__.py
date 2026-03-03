40d0cf7a1b8a83b56fbdc12201e7f13cf3690a24
Simon Niederberger <s.niederberger@hotmail.com>
2026-03-03 19:27:22 +0100

    use dynamic versioning. Better pyproject

--- src/busdayaxis/__init__.py
+++ src/busdayaxis/__init__.py
@@ -1,12 +1,8 @@
 # Copyright (c) Simon Niederberger.
 # Distributed under the terms of the Modified BSD License.
 
-# Must import __version__ first to avoid errors importing this file during the build process.
-# See https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
-from ._version import __version__  # noqa
 
 from ._locator import BusdayLocator
 from ._scale import BusdayScale, register_scale
-
 
 __all__ = ["BusdayScale", "BusdayLocator", "register_scale"]
--- src/busdayaxis/_version.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Simon Niederberger.
-# Distributed under the terms of the Modified BSD License.
-
-version_info = (0, 1, 0)
-__version__ = ".".join(map(str, version_info))
