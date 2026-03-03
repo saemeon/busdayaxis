ef4fccababea685e2d243a8fea3ef0e5a58b13de
Simon Niederberger <s.niederberger@hotmail.com>
2026-03-03 17:26:53 +0100

    Deployed 7cdeec6 with MkDocs version: 1.6.1

---
+++ generated/gallery/plot_busday.py
@@ -0,0 +1,64 @@
+"""
+Business Day Scale Example
+==========================
+
+This example demonstrates how to use the business day scale in matplotlib.
+"""
+
+# %%
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import mplbusdayaxis as bda
+
+dates = pd.date_range("2026-01-01", periods=25, freq="D")
+values = range(len(dates))
+
+
+# %%
+# Linear Scale Example
+fig, ax = plt.subplots()
+
+ax.plot(dates, values)
+ax.set_xscale("linear")
+ax.tick_params(axis="x", rotation=90)
+
+plt.show()
+
+# %%
+# Business Day Scale Example
+fig, ax = plt.subplots()
+
+ax.plot(dates, values)
+ax.set_xscale("busday")
+ax.tick_params(axis="x", rotation=90)
+
+plt.show()
+
+# %%
+# Business Day Scale Example
+fig, ax = plt.subplots()
+
+ax.plot(dates, values)
+ax.set_xscale(bda.BusdayScale(ax))
+ax.tick_params(axis="x", rotation=90)
+
+plt.show()
+
+# %%
+# Business Day Scale Example with holidays
+fig, ax = plt.subplots()
+
+ax.plot(dates, values)
+ax.set_xscale(bda.BusdayScale(ax, holidays=["2026-01-01", "2026-01-08"]))
+ax.tick_params(axis="x", rotation=90)
+plt.show()
+
+# %%
+# Business Day Scale Example with holidays
+fig, ax = plt.subplots()
+
+ax.plot(dates, values)
+ax.set_xscale("busday", holidays=["2026-01-01", "2026-01-08"])
+ax.tick_params(axis="x", rotation=90)
+plt.show()
