76b5460f2e44f2e67d0491809f01e9c408730a86
Simon Niederberger <s.niederberger@hotmail.com>
2026-03-03 16:32:20 +0100

    fix tests

---
+++ tests/test_locator.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+def test_busday_locator_import():
+    from mplbusdayaxis._locator import BusdayLocator
+
+    locator = BusdayLocator()
+    assert locator is not None
+
+
+def test_busday_locator_with_axis():
+    import matplotlib.pyplot as plt
+
+    from mplbusdayaxis._locator import BusdayLocator
+
+    fig, ax = plt.subplots()
+    locator = BusdayLocator()
+    locator.set_axis(ax.xaxis)
+
+    plt.close(fig)
---
+++ tests/test_scale.py
@@ -0,0 +1,39 @@
+import pytest
+
+
+def test_import():
+    import mplbusdayaxis
+
+    assert hasattr(mplbusdayaxis, "BusdayScale")
+    assert hasattr(mplbusdayaxis, "BusdayLocator")
+
+
+def test_busday_scale_registration():
+    import mplbusdayaxis as mplbusdayaxis
+
+    mplbusdayaxis.register_scale()
+
+
+def test_busday_scale_with_holidays():
+    import matplotlib.pyplot as plt
+
+    holidays = ["2024-01-01", "2024-12-25"]
+
+    fig, ax = plt.subplots()
+    ax.set_xscale("busday", holidays=holidays)
+
+    plt.close(fig)
+
+
+def test_plot_dates():
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    dates = pd.date_range("2024-01-01", periods=10, freq="D")
+    values = range(len(dates))
+
+    fig, ax = plt.subplots()
+    ax.plot(dates, values)
+    ax.set_xscale("busday")
+
+    plt.close(fig)
