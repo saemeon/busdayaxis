a8cbae8b3aaaa487defe495cf7d05b68983d7a82
Simon Niederberger <s.niederberger@hotmail.com>
2026-03-03 15:17:16 +0100

    add register function

--- src/mplbusdayaxis/__init__.py
+++ src/mplbusdayaxis/__init__.py
@@ -3,8 +3,11 @@ from matplotlib import scale as mscale
 from ._locator import BusdayLocator
 from ._scale import BusdayScale
 
-mscale.register_scale(BusdayScale)
 
-__all__ = ["BusdayScale", "BusdayLocator"]
+def register_scale():
+    mscale.register_scale(BusdayScale)
+
+
+__all__ = ["BusdayScale", "BusdayLocator", "register_scale"]
 
 __version__ = "0.1.0"
